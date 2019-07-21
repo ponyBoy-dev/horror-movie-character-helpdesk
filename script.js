1a58e4ddbc4ee7503381639e1338c0bd7de2a7b7
ponyBoy <20494303+ponyBoy-dev@users.noreply.github.com>
2019-07-21 21:19:06 +0200

    files uploading

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById("putaindebouton").addEventListener('click', function(){
+    //document.getElementById("titre").innerHTML = "RUN!"
+    window.location.href = "run.html"
+})
+
